51bd5c5d8b889b21e2bbb8bef3757e8df3053099
Berlin2611 <juli.mora.5667@gmail.com>
2020-03-29 18:48:02 -0500

    Terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { PostServices } from './posts.service';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers : [PostServices]
 })
 export class AppComponent {
-  title = 'myFirstApp';
+  title : string;
+  email : string;
+  hobbies : string[];
+  showHibbies : boolean;
+  posts :IPost[];
+  
+  constructor(private postService:PostServices){
+    this.title = "Angular App";
+    this.email = "juli.mora@correo.com";
+    this.hobbies = ['run', 'write', 'drink'];
+    this.showHibbies = false;
+    this.postService.getPost().subscribe(posts=>{
+      this.posts = posts;   
+    })
+  }
+  showHobbies(){
+    this.showHibbies = !this.showHibbies;
+  }
+
+  newHobby(hobby){
+    this.hobbies.push(hobby.value);
+    hobby.value = '';
+    return false;
+
+  }
+ 
+}
+interface IPost {
+  id: string;
+  title: string;
+  body : string;
 }
